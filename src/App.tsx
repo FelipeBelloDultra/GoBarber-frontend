4f9207dea25fa7dcf19740808ff7f67582371a09
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-07-02 10:30:47 -0300

    Iniciando context

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
-
 import GlobalStyles from './styles/GlobalStyles';
+
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
-    {/* <SignUp /> */}
+    <AuthContext.Provider value={{ name: '' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyles />
   </>
 );
